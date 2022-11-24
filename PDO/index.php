f0780ebe69042a9bcf7c63357f25c38fe278c015
LohainyOliveira <oliveiradossantoslohainy@gmail.com>
2022-11-23 23:10:08 -0300

    update 3

---
+++ PDO/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '_parts/_linkCSS.php'; ?>
+    <title>Página Inicial Do Projeto PW</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+    <?php include_once '_parts/_header.php'; ?>
+    <div class="p-5 text-center bg-white" style="margin-top: 58px;">
+
+    </div>
+    <?php include '_parts/_linkJS.php'; ?>
+
+</div>
+</body>
+
+</html>
